7846ba956925a8f7cee43b4c5f651a89e5767f52
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-16 15:38:17 +0530

    [Muskan] Added : This program simulate stopwatch for measuring the time that elapses between the start and end clicks.

---
+++ StopWatchProgram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Pragrams
+{
+    public class StopWatchProgram
+    {
+        public static void CalElapseTime()
+        {
+            Console.WriteLine("Press any key to start the stopwatch...");
+            Console.ReadKey();
+
+            DateTime StartTime = DateTime.Now;
+
+            Console.WriteLine("\nPress any key to stop the stopwatch.... ");
+            Console.ReadKey();
+
+            DateTime EndTime = DateTime.Now;
+
+            TimeSpan ElapsedTime = EndTime - StartTime;
+
+            Console.WriteLine("\nElapsed time: " + ElapsedTime.ToString(@"hh\:mm\:ss\.fff"));
+            Console.ReadKey();
+        }
+    }
+}
