7e288a8405fc7de98da69b35bf6de4cc6651ca3f
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-16 15:23:48 +0530

    [Muskan] Added : When the number is divided greater than 1 and divided by 1 or itself is referred to as the Prime number. 0 and 1 are not counted as prime numbers. All the even numbers can be divided by 2, so 2 is the only even prime minister.

---
+++ PrimeNumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Pragrams
+{
+    public class PrimeNumber
+    {
+        public static void CalcPrimeNumber()
+        {
+            Console.WriteLine("Enter a number ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            bool IsPrimeNumber = true;
+
+            if (num < 0)
+            {
+                IsPrimeNumber = false;
+            }
+            else
+            {
+               for (int i = 2; i <= Math.Sqrt (num); i++)
+                {
+                    if (num % i == 0)
+                    {
+                        IsPrimeNumber = false;
+                        break;
+                    }  
+                }
+            }
+            if (IsPrimeNumber)
+            {
+                Console.WriteLine("{0} is Prime Number.", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Prime Number.", num);
+            }
+        }
+    }
+}
