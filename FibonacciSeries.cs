199620d1c6ad3bf35825d5b76df5cd957c8e7653
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-16 14:27:18 +0530

    [Muskan] Added : Fibonacci series is a special type of series in which the next term is the sum of the previous two terms. For example, if 0 and 1 are the two previous terms in a series, then the next term will be 1(0+1).

---
+++ FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Pragrams
+{
+    public class FibonacciSeries
+    {
+        public static void Display()
+        {
+            int n1 = 0, n2 = 1, n3, i, number;
+            Console.WriteLine("Enter the number of elements : ");
+            number = int.Parse(Console.ReadLine());
+            Console.WriteLine(n1 + " " + n2 + " ");
+
+            for (i = 2; i < number; i++)
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(n3 + " " );
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
