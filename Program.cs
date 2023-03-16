529987f9bd568f5e208ede36c820d84f8864e1c7
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-16 15:33:23 +0530

    [Muskan] Added : This program simulates random process of distinct coupon numbers.

--- Program.cs
+++ Program.cs
@@ -16,38 +16,38 @@ namespace Logical_Pragrams
             // FibonacciSeries.Display();
             //ReverseNumber.FindReverseNumber();
             //  PrimeNumber.CalcPrimeNumber();
-            StopWatchProgram.CalElapseTime();
+            //StopWatchProgram.CalElapseTime();
 
 
-           // int[] couponNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // Replace with your own distinct coupon numbers
-            //int distinctCount = GetDistinctCouponCount(couponNumbers);
-            //Console.WriteLine($"Total random number needed to have all distinct numbers: {(int)GetDistinctCouponCount(couponNumbers)}");
+            int[] couponNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // Replace with your own distinct coupon numbers
+            int distinctCount = GetDistinctCouponCount(couponNumbers);
+            Console.WriteLine($"Total random number needed to have all distinct numbers: {(int)GetDistinctCouponCount(couponNumbers)}");
 
 
         }
 
-        //private static int GetDistinctCouponCount(int[] couponNumbers)
-        //{
+        private static int GetDistinctCouponCount(int[] couponNumbers)
+        {
 
-        //    int distinctCount = 0;
-        //    bool[] isCouponSelected = new bool[couponNumbers.Length];
-        //    while (Array.IndexOf(isCouponSelected, false) != -1)
-        //    {
-        //        int randomNumber = GenerateRandomNumber(couponNumbers.Length);
-        //        if (!isCouponSelected[randomNumber])
-        //        {
-        //            isCouponSelected[randomNumber] = true;
-        //            distinctCount++;
-        //        }
-        //    }
-        //    return distinctCount;
+            int distinctCount = 0;
+            bool[] isCouponSelected = new bool[couponNumbers.Length];
+            while (Array.IndexOf(isCouponSelected, false) != -1)
+            {
+                int randomNumber = GenerateRandomNumber(couponNumbers.Length);
+                if (!isCouponSelected[randomNumber])
+                {
+                    isCouponSelected[randomNumber] = true;
+                    distinctCount++;
+                }
+            }
+            return distinctCount;
 
-        //}   
-        //public static int GenerateRandomNumber(int maxNumber)
-        //{
-        //        Random random = new Random();
-        //        return random.Next(maxNumber);
-        //}
-        
+        }
+        public static int GenerateRandomNumber(int maxNumber)
+        {
+            Random random = new Random();
+            return random.Next(maxNumber);
+        }
+
     }
 }
